9c8c29342da180008fe0958706e7c00603f6f38e
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-06 15:24:34 -0600

    Lesson 12 - useEffect Hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import NewSongForm from './NewSongForm';
 
@@ -10,11 +10,18 @@ const SongList = () => {
         { title: 'This Wild Darkness', id: 3 }
     ]);
 
+    const [age, setAge] = useState(20);
+
     const addSong = (title) => {
         //setSongs is from above it is used to modify the 'songs' list/array. The 3 periods is to pull the songs from above. 
         setSongs([...songs, { title, id: uuidv4() }])
     }
-    
+    useEffect(() => {
+        console.log('useEffect hook ran', songs);
+    }, [songs])
+    useEffect(() => {
+        console.log('useEffect hook ran', age);
+    }, [age])
     return (
         <div className='song-list'>
             <ul>
@@ -23,6 +30,7 @@ const SongList = () => {
                 })}
             </ul>
             <NewSongForm addSong={addSong}/>
+            <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
         </div>
         );
     }
