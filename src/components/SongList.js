e38fbadc94960ef0cc200bdbc9319b9628d0edf0
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-06 11:46:38 -0600

    Lesson 9 - Intro to Hooks

---
+++ src/components/SongList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+    render() { 
+        return (
+            <div className='song-list'>
+                <ul>
+                    <li>This Wild Darkness</li>
+                    <li>Memory Gospel</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default SongList;
